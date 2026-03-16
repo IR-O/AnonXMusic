3963b27d3401f9d4bb00b207cca55cb4442862ab
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-03-16 20:46:19 +0530

    Update _inline.py
    Signed-off-by: Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>

--- anony/helpers/_inline.py
+++ anony/helpers/_inline.py
@@ -148,15 +148,15 @@ class Inline:
                 self.ikb(text=lang["channel"], url=config.SUPPORT_CHANNEL),
             ],
         ]
-        if private:
-            rows += [
-                [
-                    self.ikb(
-                        text=lang["source"],
-                        url="https://github.com/AnonymousX1025/AnonXMusic",
-                    )
-                ]
-            ]
+#        if private:
+#            rows += [
+#                [
+#                    self.ikb(
+#                        text=lang["source"],
+#                        url="https://github.com/AnonymousX1025/AnonXMusic",
+#                    )
+#                ]
+#            ]
         else:
             rows += [[self.ikb(text=lang["language"], callback_data="language")]]
         return self.ikm(rows)
